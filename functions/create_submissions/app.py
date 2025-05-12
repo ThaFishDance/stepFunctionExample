ad1dba2ab3ce00ee38e1bf45f3e7d5dd1f51cc98
ThaFishDance <chuffman91@gmail.com>
2025-05-12 09:40:21 -0500

    update

--- functions/create_submissions/app.py
+++ functions/create_submissions/app.py
@@ -11,38 +11,38 @@ load_dotenv()
 BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 
-def create_inner_zip(index, number, name1, name2, json_count=1):
+def create_inner_zip(index, doc_code, json_count=1):
     inner_zip_buffer = io.BytesIO()
     with zipfile.ZipFile(inner_zip_buffer, 'w', zipfile.ZIP_DEFLATED) as inner_zip:
         for j in range(json_count):
             result_data = {
-                "number": number + index + j,
-                "square": (number + index + j) ** 2,
-                "processed_by": [name1, name2]
+                "doc_code": doc_code
             }
-            inner_zip.writestr(f"result{index}_{j}.json", json.dumps(result_data).encode("utf-8"))
+            inner_zip.writestr(f"result_{index}_{j}.json", json.dumps(result_data).encode("utf-8"))
     inner_zip_buffer.seek(0)
     return f"inner_result_{index}.zip", inner_zip_buffer.read()
 
 
-def get_s3_key(number):
+def get_s3_key(job_id, number):
+    # TODO: get csv from s3, the s3 key is the job_id
+    #  and row data from row[number]
     return "nested/outer_result.zip"
 
 
 def lambda_handler(event, context):
     s3 = boto3.client("s3")
-    number = event["number"]
-    KEY_NAME = get_s3_key(number)
-    name1 = event["job_id"]
-    count = int(event.get("count", 1))  # default to 1 if not provided
+    number = event["number"] # comes from the map item value
+    job_id = event["job_id"]
+    transmission_zip_name = get_s3_key(job_id, number)
+    submission_count = int(event.get("submission_count", 1))  # default to 1 if not provided
     json_count = int(event.get("json_count", 1))
-    print(f'test count: {count}')
+    print(f'submission_count: {submission_count}')
 
     inner_zip_results = []
     with ThreadPoolExecutor(max_workers=3) as executor:  # max 3 concurrent workers
         futures = [
-            executor.submit(create_inner_zip, i, number, name1, name2, json_count)
-            for i in range(count)
+            executor.submit(create_inner_zip, i, number,  json_count)
+            for i in range(submission_count)
         ]
         for future in futures:
             inner_zip_results.append(future.result())
@@ -57,7 +57,7 @@ def lambda_handler(event, context):
 
     s3.put_object(
         Bucket=BUCKET_NAME,
-        Key=KEY_NAME,
+        Key=transmission_zip_name,
         Body=outer_zip_buffer.getvalue(),
         ContentType="application/zip"
     )
@@ -65,5 +65,5 @@ def lambda_handler(event, context):
     return {
         "message": f"{len(inner_zip_results)} inner ZIPs uploaded successfully",
         "s3_bucket": BUCKET_NAME,
-        "s3_key": KEY_NAME
+        "s3_key": transmission_zip_name
     }
